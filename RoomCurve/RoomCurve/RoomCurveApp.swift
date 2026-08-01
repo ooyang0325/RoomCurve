1ac3d5e0574c57d149e91347e45a2bea2e7c3001
oo_yang <b11901064@ntu.edu.tw>
2026-08-01 18:59:35 +0800

    Create Xcode project

---
+++ RoomCurve/RoomCurve/RoomCurveApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomCurveApp.swift
+//  RoomCurve
+//
+//  Created by 楊詠翔 on 2026/8/1.
+//
+
+import SwiftUI
+
+@main
+struct RoomCurveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
